edfd0f82a7610dede49e6989d48c6ba1decf22ca
0987eyhdf <157504303+0987eyhdf@users.noreply.github.com>
2024-01-24 00:16:20 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oc7Ap6pg7V-fuvswIpldv0tfef58',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'aP97PGD6oX2jmcxltwlaQ06vgDTrV901taR4nLFfGYo',
+      useTemplateId: '3TEf4u8seG9k9wfcCCSLgddeq9oSmNJ5wnTDDVl_Ciw',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '6-29',
       festivals: [
